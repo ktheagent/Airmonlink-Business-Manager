4918e6660bd8e525262944e30bd4760b8037979b
ktheagent <110701561+ktheagent@users.noreply.github.com>
2026-07-18 20:27:36 +0000

    Centralize commercial URLs and trial settings

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -4,5 +4,10 @@ class AppConstants {
   static const defaultCurrency = 'GHS';
   static const databaseName = 'airmonlink_business_manager.db';
 
+  static const purchaseUrl = 'https://buy.airmonlink.com';
+  static const licensingApiUrl = 'https://license.airmonlink.com';
+  static const supportUrl = 'https://airmonlink.com/support';
+  static const trialDays = 14;
+
   AppConstants._();
 }
